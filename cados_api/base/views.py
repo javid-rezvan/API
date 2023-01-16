084e1405eee3a8e09d77f4a0b92e1610b10fbb79
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-01-16 16:25:19 +0330

    Update views.py

--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def endpoins(request):
     data=['/advocates','advocates/:username']
+    return JsonResponse(data,safe=False)
+
+def advocate_list(request):
+    data=['Dennis','Tadas','Max']
+    return JsonResponse(data,safe=False)
+
+def advocate_details(request,username):
+    data=username
     return JsonResponse(data,safe=False)
