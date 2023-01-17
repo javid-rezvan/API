35cf751881554738970a347778b1cde341725565
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-01-17 12:50:36 +0330

    Update views.py

--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Advocate
+from .serializers import AdvocateSerializer
+from django.db.models import Q
 # Create your views here.
+@api_view(['GET'])
 def endpoins(request):
     data=['/advocates','advocates/:username']
-    return JsonResponse(data,safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def advocate_list(request):
-    data=['Dennis','Tadas','Max']
-    return JsonResponse(data,safe=False)
+    
+    #data=['Dennis','Tadas','Max']
+    query=request.GET.get('query')
+    print('Query:',query)
+    if query==None:
+        query=''
+        
+    advocates=Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+    serializer=AdvocateSerializer(advocates,many=True)
+    return Response(serializer.data)
 
-def advocate_details(request,username):
-    data=username
-    return JsonResponse(data,safe=False)
+@api_view(['GET'])
+def advocate_detail(request,username):
+    advocate=Advocate.objects.get(username=username)
+    serializer=AdvocateSerializer(advocate,many=False)
+    return Response(serializer.data)
