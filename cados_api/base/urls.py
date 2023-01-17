d8a26caccf23bf424915a6599d86833400149cc0
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-01-17 15:46:54 +0330

    Update urls.py

--- cados_api/base/urls.py
+++ cados_api/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name='base'
+
 urlpatterns=[
     path('',views.endpoins),
-    path('advocates/',views.advocate_list),
+    path('advocates/',views.advocate_list,name="advocates"),
     path('advocates/<str:username>/',views.advocate_detail),
 ] 
