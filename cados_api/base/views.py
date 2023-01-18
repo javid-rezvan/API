bcd37b6c2c198adc9a49d8212fbb07c03c94ff8c
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-01-18 12:43:45 +0330

    Update views.py

--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Advocate
-from .serializers import AdvocateSerializer
+from .models import Advocate,Company
+from .serializers import AdvocateSerializer,CompanySerializer
 from django.db.models import Q
+from rest_framework.views import APIView
 # Create your views here.
 
 @api_view(['GET'])
@@ -33,21 +34,53 @@ def advocate_list(request):
     return Response(serializer.data)
 
 
-@api_view(['GET','PUT','DELETE'])
-def advocate_detail(request,username):
-      advocate=Advocate.objects.get(username=username)
-      if request.method=='GET':
-         serializer=AdvocateSerializer(advocate,many=False)
-         return Response(serializer.data)
+# @api_view(['GET','PUT','DELETE'])
+# def advocate_detail(request,username):
+#       advocate=Advocate.objects.get(username=username)
+#       if request.method=='GET':
+#          serializer=AdvocateSerializer(advocate,many=False)
+#          return Response(serializer.data)
  
-      if request.method=='PUT':
-         advocate.username=request.data['username']
-         advocate.bio=request.data['bio']
-         advocate.save()
-         serializer=AdvocateSerializer(advocate,many=False)
-         return Response(serializer.data)
+#       if request.method=='PUT':
+#          advocate.username=request.data['username']
+#          advocate.bio=request.data['bio']
+#          advocate.save()
+#          serializer=AdvocateSerializer(advocate,many=False)
+#          return Response(serializer.data)
      
-      if request.method=='DELETE':
-          advocate.delete()   
-          return Response('user was deleted')
+#       if request.method=='DELETE':
+#           advocate.delete()   
+#           return Response('user was deleted')
 
+class AdvocateDetail(APIView):
+    
+    def get_object(self,username):
+        try:
+            return Advocate.objects.get(username=username)
+        except Advocate.DoesNotExist:
+              raise JsonResponse('Advocate dosenot exist')               
+    def get(self,request,username):
+        advocate=self.get_object(username)
+        serializer=AdvocateSerializer(advocate,many=False)
+        return Response(serializer.data)
+    
+    def put(self,request,username):
+        advocate=self.get_object(username)
+        advocate.username=request.data['username']
+        advocate.bio=request.data['bio']
+        advocate.save()
+        serializer=AdvocateSerializer(advocate,many=False)
+        return Response(serializer.data)
+    
+    def delete(self,request,username):
+         advocate=self.get_object(username)
+         advocate.delete()   
+         return Response('user was deleted')
+        
+@api_view(['GET'])        
+def companies_list(request):
+    companies=Company.objects.all()
+    serializer=CompanySerializer(companies,many=True)
+    return Response(serializer.data)
+    
+    
