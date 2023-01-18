15f34f1748736fbb484c45ce41b316d8756d81a9
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-01-18 12:43:42 +0330

    Update urls.py

--- cados_api/base/urls.py
+++ cados_api/base/urls.py
@@ -6,5 +6,12 @@ app_name='base'
 urlpatterns=[
     path('',views.endpoins),
     path('advocates/',views.advocate_list,name="advocates"),
-    path('advocates/<str:username>/',views.advocate_detail),
+    # path('advocates/<str:username>/',views.advocate_detail),
+    path('advocates/<str:username>/',views.AdvocateDetail.as_view()),
+    
+    #companies/
+    path('companies/',views.companies_list),
+    #companies/:id
+   
+    
 ] 
