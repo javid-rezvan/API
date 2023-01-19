003cdf4eb4179bae69c844cda6e1c16abfe26dfd
javid-rezvan <javid.rezvan@gmail.com>
2023-01-19 20:51:32 +0330

    Update urls.py

--- cados_api/base/urls.py
+++ cados_api/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, 
+    TokenRefreshView,
 )
 
 app_name='base'
@@ -19,5 +20,6 @@ urlpatterns=[
     
     #Authentication
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
        
 ] 
