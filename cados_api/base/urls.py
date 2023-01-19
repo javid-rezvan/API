4ced4905ee310aed9bb3694d56bea1eee0471c44
javid-rezvan <javid.rezvan@gmail.com>
2023-01-19 15:03:52 +0330

    Update urls.py

--- cados_api/base/urls.py
+++ cados_api/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, 
+)
+
 app_name='base'
 
 urlpatterns=[
@@ -12,6 +16,8 @@ urlpatterns=[
     #companies/
     path('companies/',views.companies_list),
     #companies/:id
-   
     
+    #Authentication
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+       
 ] 
